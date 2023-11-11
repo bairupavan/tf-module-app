44f15cfffc87ecd3599cfabdad7510c53aa86ea7
pavanbairu <pavan.bairu1@gmail.com>
2023-11-11 20:23:24 +0530

    creating the app instances with autoscaling and launch templates

--- main.tf
+++ main.tf
@@ -1,5 +1,69 @@
-resource "aws_instance" "instance" {
-  ami = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-  subnet_id = var.subnet_id
+resource "aws_security_group" "sg" {
+  name        = "${var.name}-${var.env}-sg"
+  description = "${var.name}-${var.env}-sg"
+  vpc_id      = var.vpc_id
+
+  # access these for only app subnets
+  ingress {
+    description = "APP"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = var.allow_app_cidr
+  }
+
+  # connecting through the ssh bastion(work station node)
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.bastion_cidr
+  }
+
+  # outside access
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "${var.name}-${var.env}-sg"
+  }
+}
+
+# creating the ec2 launch template
+resource "aws_launch_template" "template" {
+  name                   = "${var.env}-${var.name}-lt"
+  image_id               = data.aws_ami.centos.image_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_autoscaling_group.asg.id]
+}
+
+# creating the auto scaling group to scale instance based on the load
+resource "aws_autoscaling_group" "asg" {
+  name                = "${var.env}-${var.name}-asg"
+  vpc_zone_identifier = var.subnet_ids
+  desired_capacity    = var.desired_capacity
+  max_size            = var.max_size
+  min_size            = var.min_size
+
+  # attaching the template for autoscaling group
+  launch_template {
+    id      = aws_launch_template.template.id
+    version = "$Latest"
+  }
+
+  # tags
+  dynamic "tag" {
+    for_each = local.asg_tags
+    content {
+      key                 = tag.key
+      propagate_at_launch = "true"
+      value               = tag.value
+    }
+  }
 }
--- vars.tf
+++ vars.tf
@@ -1,2 +1,11 @@
 variable "instance_type" {}
-variable "subnet_id" {}
+variable "subnet_ids" {}
+variable "vpc_id" {}
+variable "env" {}
+variable "name" {}
+variable "bastion_cidr" {}
+variable "allow_app_cidr" {}
+variable "desired_capacity" {}
+variable "max_size" {}
+variable "min_size" {}
+variable "tags" {}
